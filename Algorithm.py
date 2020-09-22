e68dc9208159caa02ef5c53afbd6dc517f4ffd9b
Pikpika-1 <59190707+Pikpika-1@users.noreply.github.com>
2020-09-22 21:04:20 +0800

    Add files via upload

---
+++ Algorithm.py
@@ -0,0 +1,9 @@
+import jieba
+import sys
+
+print(sys.argv)
+# orig_path = 'E:\test\orig.txt'
+# orig_add_file = 'E:\test\orig_0.8_add'
+orig_file = open(orig_path, 'r', encoding="utf-8")
+orig_add_file = open(orig_add_path, 'r', encoding="utf-8")
+
